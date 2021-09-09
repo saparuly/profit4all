3d12e0b6f687f86b9f9b4396e8ecca1de6e4ab8a
saparuly <rizkysaparully@gmail.com>
2021-09-09 17:45:49 +0700

    deploy profit4all to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import profileImg from "./img/profile.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React APP</h1>
+      <h2>Author : Rizky Saparuly</h2>
+      <img src={profileImg} alt="Profile-img" />
     </div>
   );
 }
